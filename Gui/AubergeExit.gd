e7aff6961785e7af347ef4de09c88a35e8aba512
Shionn <shionn@gmail.com>
2026-09-22 13:46:14 +0200

    Proto Dungeon 04

--- Gui/AubergeExit.gd
+++ Gui/AubergeExit.gd
@@ -15,6 +15,10 @@ func _on_dungeon_03_pressed() -> void:
 	gui._close()
 	gui.openTransition(func():_goTo("res://Dungeons/03/Dungeon03.tscn"))
 
+func _on_dungeon_04_pressed() -> void:
+	gui._close()
+	gui.openTransition(func():_goTo("res://Dungeons/04/Dungeon04.tscn"))
+
 func _goTo(path : String) -> void : 
 	var dungeon = $/root/World/Dungeon as Node3D
 	dungeon.name = "old"
